87530830969e9f28ff808283556bb0760fcf5515
AlexandrOAV <a0617011995@gmail.com>
2023-05-17 14:33:42 +0300

    Update task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,42 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const listEl = document.querySelector('.gallery');
+const addMarkup = creatingListPictures(galleryItems);
+function closeModalEscape(instance) {
+    document.addEventListener("keydown", event => {
+    if (event.key !== 'Escape') {
+    return}
+    instance.close();
+});}
+function creatingListPictures(gallaryArray) {
+  return gallaryArray
+    .map(({preview, original, description}) =>{
+        return ` <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li> `
+    })
+    .join('\n')
+}
+listEl.insertAdjacentHTML('beforeend', addMarkup);
+listEl.addEventListener('click', clickOnPicture)
+function clickOnPicture(event) {
+    event.preventDefault();
+    if (event.target.className !== "gallery__image")  {
+        return;
+    }
+    const instance  = basicLightbox.create(`<img
+      src="${event.target.dataset.source}"
+    />`)
+    instance.show();
+    closeModalEscape(instance);
+}
+
+
